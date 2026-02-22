e97fddfd7c28abbbe8b7dfe03f4889ee0df0a4bc
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-23 01:25:25 +0530

    show the working and use of middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './src/config/dbConfig.js';
+import { createPost } from './src/controllers/postController.js';
 
 const PORT = 3000; // port number
 
@@ -8,8 +9,26 @@ const app = express(); // create express app server instance
 app.get('/ping', (req, res) => {
     return res.json({ message: 'pong' });
 });
+// here we use this so that to show how middleware chaining can be use
+function middleware1(req,res,next)
+{
+    console.log('middleware1');
+    next();
+}
+function middleware2(req,res,next)
+{
+    console.log('middleware2');
+    next();
+}
+function middleware3(req,res,next)
+{
+    console.log('middleware3');
+    next();
+}
+app.post('/post',middleware1,middleware2,middleware3,createPost)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
+
