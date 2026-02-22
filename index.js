73ba1e29ee9819aa5d1552a47093ada629d9d996
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-23 01:53:08 +0530

    handling the problem of passing data using request body

--- index.js
+++ index.js
@@ -7,25 +7,17 @@ const PORT = 3000; // port number
 const app = express(); // create express app server instance
 
 app.get('/ping', (req, res) => {
+    // this is how we can see the content passed using query like ? in url.
+     console.log(req.query);
+    console.log(req.body);
     return res.json({ message: 'pong' });
 });
 // here we use this so that to show how middleware chaining can be use
-function middleware1(req,res,next)
-{
-    console.log('middleware1');
-    next();
-}
-function middleware2(req,res,next)
-{
-    console.log('middleware2');
-    next();
-}
-function middleware3(req,res,next)
-{
-    console.log('middleware3');
-    next();
-}
-app.post('/post',middleware1,middleware2,middleware3,createPost)
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+app.post('/post',createPost)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
