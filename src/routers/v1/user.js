c0e90f04a14b3c39e3b4341a77961635c64aa662
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-24 10:15:08 +0530

    Added the signIn mechanism and the use of jwt token and use of package jsonwebtoken by npm

--- src/routers/v1/user.js
+++ src/routers/v1/user.js
@@ -1,12 +1,13 @@
 // After /users the remainaing part of url is handled here
 import express from 'express';
-import { getProfile,signup } from '../../controllers/userController.js';
-import { zodSignupSchema } from '../../validators/zodSignUpValidator.js';
+import { getProfile,signup, signin } from '../../controllers/userController.js';
+import { zodSignupSchema, } from '../../validators/zodSignUpValidator.js';
+import { zodSigninSchema } from '../../validators/zodSigninSchema.js';
 import { validate } from '../../validators/zodValidator.js';
 const router = express.Router();
 
 router.get('/profile', getProfile);
 router.post('/signup', validate(zodSignupSchema), signup);
-
+router.post('/signin', validate(zodSigninSchema), signin);
 
 export default router;
