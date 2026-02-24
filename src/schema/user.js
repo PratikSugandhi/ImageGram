c2236335df922f1fa79c96a3f8238038539bcdd3
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-24 09:47:54 +0530

    Added the signUp mechanism and hashing of the password

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 // creating the schema
+import bcrypt from 'bcrypt'
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        minLength: 5
+        minLength: 6
     },
     email: {
         type: String,
@@ -27,7 +28,27 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+// pre is like a trigger which is envoked when an event occcures like just before signing or like after signing.
+userSchema.pre('save', function modifyPassword(next) {
+    // incoming user object
+    const user = this; // object with plain password
+
+    const SALT = bcrypt.genSaltSync(9);
+
+    // hash password
+
+    const hashedPassword = bcrypt.hashSync(user.password, SALT);
+
+    // replace plain password with hashed password
+    user.password = hashedPassword;
+
+    next();
+});
+
+
 // converting the schema into collections.
 const user = mongoose.model("User", userSchema); // user collection
+
+
 
 export default user;
