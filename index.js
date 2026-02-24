11b150e4674773eae83a4165dd24bf4064823928
Pratik Sugandhi <pratiksugandhi4@gmail.com>
2026-02-24 19:30:02 +0530

    Added the mechanism for showing ip address while calling ping using ip npm package and also removed auth in calling ping so ne need to setup agian again

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { isAuthenticated } from './src/middlewares/authMiddleware.js';
 import swaggerUi from 'swagger-ui-express'
 import swaggerJSDoc from 'swagger-jsdoc';
 import { options } from './src/utilies/swaggerOptions.js';
+import ip from 'ip'
 
 const PORT = 3000; // port number
 
@@ -14,12 +15,14 @@ const app = express(); // create express app server instance
 
 const upload = multer();
 
-app.get('/ping',isAuthenticated, (req, res) => {
+// Removed this authenticated so that when we deploy so while run this no any authentication is asked and also we write ping so that we ccan check whaeter after deploying is server is up or not.
+app.get('/ping',/*isAuthenticated,*/ (req, res) => {
     // this is how we can see the content passed using query like ? in url.
      console.log(req.query);
     console.log(req.body);
     console.log(req.user);
-    return res.json({ message: 'pong' });
+    const ipaddr = ip.address();
+    return res.json({ message: 'pong' + ipaddr });
 });
 // here we use this so that to show how middleware chaining can be use
 app.use(express.json());
@@ -35,4 +38,8 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 });
+
+
+// client. -----> LB ------> server1
+//                   ------> server2       
 
